bf8f519f49973c5a84921df7ea6ef9aab63bf620
ArtusAbelard <tribelha@gmail.com>
2024-02-20 21:32:57 +0100

    try create div for panier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Navbar(props) {
-    console.log(props.info.montres_de_luxe[0].marque);
     
+    let ewa = ["edgf","zgfd","gfdgdf","gfgd"]
   return (
     <div className='pb-10 w-full'>
 
@@ -26,13 +27,23 @@ export default function Navbar(props) {
                     <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
                         <div className="card-body">
                             <span className="font-bold text-lg">{props.panierprix.length} Items</span>
-                            <span className="text-info">Subtotal: $999</span>
+                            <span className="text-info">Subtotal:</span>
                             
                             <div className="card-actions">
                                 <button className="btn btn-primary btn-block">
                                  <h1>{props.info.montres_de_luxe[0].marque} {props.info.montres_de_luxe[0].nom}</h1> 
                                  <p className='text-red-600'>{props.info.montres_de_luxe[0].prix}$</p>  
                                 </button>
+                                {
+                                    ewa.map((element,id)=>{
+                                        return (
+                                            <button className="btn btn-primary btn-block">
+                                            <h1>{props.info.montres_de_luxe[0].marque} {props.info.montres_de_luxe[0].nom}</h1> 
+                                            <p className='text-red-600'>{props.info.montres_de_luxe[0].prix}$</p>  
+                                            </button>
+                                        )
+                                    })
+                                }
                             </div>
                             <div className="card-actions">
                                 <button className="btn btn-primary btn-block"></button>
