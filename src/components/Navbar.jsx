a106664e5ea3600fb330e61de4a5ba4649cb6cd3
SirMarouane <m.elbattioui@hotmail.com>
2024-02-20 14:49:56 +0100

    changing panier 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,20 +16,21 @@ export default function Navbar(props) {
             
             <div className="flex-none">
                 <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                    <img src={props.sac} width="60%" alt="" /><span className="badge badge-sm indicator-item">4</span>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                        <div className="indicator">
+                            <img src={props.sac} width="60%" alt="" /><span className="badge badge-sm indicator-item">4</span>
+                        </div>
                     </div>
-                </div>
-                <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div className="card-body">
-                    <span className="font-bold text-lg">8 Items</span>
-                    <span className="text-info">Subtotal: $999</span>
-                    <div className="card-actions">
-                        <button className="btn btn-primary btn-block">View cart</button>
+                    <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                        <div className="card-body">
+                            <span className="font-bold text-lg">8 Items</span>
+                            <span className="text-info">Subtotal: $999</span>
+                            
+                            <div className="card-actions">
+                                <button className="btn btn-primary btn-block">View cart</button>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                </div>
                 </div>
             </div>
                 
