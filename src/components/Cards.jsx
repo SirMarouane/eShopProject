74ebbc40b4f56d0a3a47c0a0fbd77f1ea6b22d94
SirMarouane <m.elbattioui@hotmail.com>
2024-02-20 12:01:04 +0100

    json

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ export default function Cards(props) {
   return (
     <div className="card w-60 bg-base-100 shadow-xl hover:bg-[#DCA54C]/55 hover:text-black hover:border-[1px] hover:border-[#DCA54C]/35 ">
         <figure className="">
-            <img src={props.img} alt="" className="rounded-xl " />
+            <img src={props.img} alt="" className="rounded-xl "/>
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">Richard Mille </h2>
