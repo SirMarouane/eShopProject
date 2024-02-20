7cb76b3bbde96b9a3fd11fa6801b02e73e55a8ba
SirMarouane <m.elbattioui@hotmail.com>
2024-02-20 10:20:45 +0100

    modif card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Cards(props) {
   return (
-    <div className="card w-60 bg-base-100 shadow-xl ">
-        <figure className="px- pt-">
-            <img src={props.img} alt="" className="rounded-xl" />
+    <div className="card w-60 bg-base-100 shadow-xl hover:bg-[#DCA54C]/55 hover:text-black hover:border-[1px] hover:border-[#DCA54C]/35 ">
+        <figure className="">
+            <img src={props.img} alt="" className="rounded-xl " />
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">Richard Mille </h2>
